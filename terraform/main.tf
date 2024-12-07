5e5ccb0132de09d2bd3b232c42926824e70b610e
root <root@ip-172-31-82-165.ec2.internal>
2024-12-07 14:56:21 +0000

    addign terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,3 @@
-#provider "aws"{
-#region       ="us-east-1"
-#access_key   ="AKIAW3MEA4PTRSSMUOD4"
-#secret_key   ="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"
-
-
 module "instance" {
 count = 0
 source = "./ec2"
